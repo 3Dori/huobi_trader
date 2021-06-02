1d3871dac14a03bd0be71fb9a07db43628cf157a
3Dori <chelvyixue@gmail.com>
2021-06-02 16:19:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import json
+import os
+from pathlib import Path
+
 from constants import *
 from trader.trader import Trader
 
 from huobi.constant import *
 
+trader = None
+
+
 def main():
-    # trader = Trader(API_KEY, SECRET_KEY, ACCOUNT_ID)
-    # trader.create_sell_queue('btcusdt', lower_price=45350, upper_price=45577, total_amount=0.001)
-    pass
+    api_config = Path(os.path.join('.', 'API_key.json'))
+    with api_config.open('rt') as handle:
+        config = json.load(handle)
+    global trader
+    trader = Trader(config['api_key'], config['secret_key'], config['account_id'])
 
 
-trader = Trader(API_KEY, SECRET_KEY, ACCOUNT_ID)
-
-if __name__ == '__main__':
-    main()
+main()
